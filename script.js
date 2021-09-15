4b495abed03cf32ae9f35882f3459e697cb26798
Sayantika Ghosh <sayantikaghosh98@gmail.com>
2021-09-15 13:51:29 +0530

    Added comments in the javascript code

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+/* str stores the value of the second operand, 
+flag has been used to check that only one operand is selected and
+prevStr stores the value of the first operand
+*/
+
 let str="", flag = false, prevStr = "";
+
+/*
+Initially all the buttons are disabled and only the AC button is enabled and 
+as the AC button is clicked zero appears on the screen and all the other buttons are enabled 
+
+*/
 
 document.querySelectorAll(".but").forEach(ele=>ele.childNodes[1].disabled = true);
 document.getElementById("ac-btn").childNodes[1].disabled = false;
@@ -7,6 +18,15 @@ document.querySelector(".ac").addEventListener("click", (event)=>{
     document.querySelector(".current-compute").textContent = event.target.dataset.ac;
     document.querySelectorAll(".but").forEach(ele => ele.childNodes[1].disabled = false);
 });
+
+/*
+
+As any number is clicked it get stored up in the str in the current-compute class div and as 
+any operator is clicked then the str value gets up in the previous-compute class div and 
+it gets stored up in the prevStr and the new numbers which are clicked gets stored up in the 
+str variable as the second operand.
+
+*/
 
 document.querySelectorAll(".num").forEach(ele=>{
     ele.addEventListener("click",event=>{
@@ -20,6 +40,10 @@ document.querySelectorAll(".num").forEach(ele=>{
         }
     });
 });
+
+/*
+Event listener for clicking the opearator button
+*/
 
 document.querySelectorAll(".op").forEach(ele=>{
     ele.addEventListener("click",event=>{
