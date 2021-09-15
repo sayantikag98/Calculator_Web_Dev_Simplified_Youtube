d014d98e5fd0a6e94ddf8149bf66a20e3635a31c
Sayantika Ghosh <sayantikaghosh98@gmail.com>
2021-09-15 15:34:16 +0530

    Multiple computation and delete part added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ flag has been used to check that only one operand is selected and
 prevStr stores the value of the first operand
 */
 
-let str="", flag = false, prevStr = "";
+let str, flag, prevStr;
 
 /*
 Initially all the buttons are disabled and only the AC button is enabled and 
@@ -17,6 +17,10 @@ document.getElementById("ac-btn").childNodes[1].disabled = false;
 document.querySelector(".ac").addEventListener("click", (event)=>{
     document.querySelector(".current-compute").textContent = event.target.dataset.ac;
     document.querySelectorAll(".but").forEach(ele => ele.childNodes[1].disabled = false);
+    document.querySelector(".previous-compute").textContent = "";
+    str = "";
+    prevStr = "";
+    flag = false;
 });
 
 /*
@@ -30,6 +34,9 @@ str variable as the second operand.
 
 document.querySelectorAll(".num").forEach(ele=>{
     ele.addEventListener("click",event=>{
+        if(event.target.dataset.num === "."){
+            str+="0";
+        }
         str+=event.target.dataset.num;
         document.querySelector(".current-compute").textContent = str;
         if((str[str.length - 2]=== "+")||(str[str.length - 2]=== "-")||(str[str.length - 2]=== "*")||(str[str.length - 2]=== "/")){
@@ -62,26 +69,59 @@ This event listener does all the actual calculation of the calculator.
 */
 
 document.querySelector(".equal").addEventListener("click",()=>{
-    document.querySelector(".previous-compute").textContent += str;
-    const operator = prevStr[prevStr.length - 1];
-    let ans = 0;
-    const op1 = parseInt(prevStr.substr(0,prevStr.length - 1)),
-    op2 = parseInt(str); 
-    if(operator === "+"){
-        ans = op1 + op2;
+    document.querySelector(".previous-compute").textContent += str; 
+    let ans = 0, op1, op2, operator = prevStr[prevStr.length - 1], operator1 = str[str.length - 1];
+    if((prevStr.length === 0) && (operator1 !== "+") &&(operator1 !== "-") && (operator1 !== "*") &&(operator1 !== "/")){
+        document.querySelector(".current-compute").textContent = str;
     }
-    else if (operator === "-"){
-        ans = op1 - op2;
+    else{
+        let op1Str = prevStr.substr(0,prevStr.length-1);
+        if(op1Str[1]===".")
+            op1 = parseFloat(op1Str);
+        else 
+            op1 = parseInt(op1Str);
+        if(str[1]===".")
+            op2 = parseFloat(str);
+        else 
+            op2 = parseInt(str);
+        if(operator === "+"){
+            ans = op1 + op2;
+        }
+        else if (operator === "-"){
+            ans = op1 - op2;
+        }
+        else if (operator === "*"){
+            ans = op1 * op2;
+        }
+        else if (operator === "/"){
+            ans = op1/op2;
+        }
+        str = ans;
+        flag = false;
+        document.querySelector(".current-compute").textContent = ans;
     }
-    else if (operator === "*"){
-        ans = op1 * op2;
-    }
-    else if (operator === "/"){
-        ans = op1/op2;
-    }
-    console.log(ans);
-    document.querySelector(".current-compute").textContent = ans;
 });
+
+/*
+
+This event handler is used to handle the clicking event of the delete key
+
+*/
+
+document.querySelector(".del").addEventListener("click",()=>{
+    str = str.substr(0, str.length-1);
+    flag = false;
+    if(str.length === 0)
+        document.querySelector(".current-compute").textContent = 0;
+    else 
+        document.querySelector(".current-compute").textContent = str;
+});
+
+document.querySelectorAll(".but").forEach(ele => {
+    ele.childNodes[1].style.opacity = "60%";
+});
+
+
 
 
     
